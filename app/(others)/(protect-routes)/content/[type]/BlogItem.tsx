daac88ec5bb0a315a31c7b720a42a14fd50fef37
autsada <autsada.t@gmail.com>
2023-09-26 14:14:54 +0700

    added delete many for blogs content

--- app/(others)/(protect-routes)/content/[type]/BlogItem.tsx
+++ app/(others)/(protect-routes)/content/[type]/BlogItem.tsx
@@ -1,6 +1,10 @@
 import React, { useCallback } from "react"
 import { useRouter } from "next/navigation"
 import { BsEye, BsEyeSlash } from "react-icons/bs"
+import {
+  MdOutlineCheckBox,
+  MdOutlineCheckBoxOutlineBlank,
+} from "react-icons/md"
 
 import { formatDate, getPostExcerpt } from "@/lib/client"
 import { useSubscribeToUpdates } from "@/hooks/useSubscribe"
@@ -8,9 +12,11 @@ import type { Publish } from "@/graphql/codegen/graphql"
 
 interface Props {
   blog: Publish
+  isSelected: boolean
+  selectItem: (id: string) => void
 }
 
-export default function BlogItem({ blog }: Props) {
+export default function BlogItem({ blog, isSelected, selectItem }: Props) {
   const isDeleting = blog.deleting
 
   const router = useRouter()
@@ -29,8 +35,20 @@ export default function BlogItem({ blog }: Props) {
       className={`relative text-sm hover:bg-gray-50 ${
         isDeleting ? "opacity-30" : "opacity-100 cursor-pointer"
       }`}
-      onClick={isDeleting ? undefined : onClickItem.bind(undefined, blog.id)}
     >
+      <th className="w-[6%] lg:w-[3%]">
+        {isSelected ? (
+          <MdOutlineCheckBox
+            size={25}
+            onClick={selectItem.bind(undefined, blog.id)}
+          />
+        ) : (
+          <MdOutlineCheckBoxOutlineBlank
+            size={24}
+            onClick={selectItem.bind(undefined, blog.id)}
+          />
+        )}
+      </th>
       <th className="w-[30%] sm:w-[20%] lg:w-[15%] xl:w-[12%] font-normal py-2 break-words">
         <div className="w-full h-full flex items-center justify-center">
           {blog.thumbnail ? (
@@ -47,7 +65,10 @@ export default function BlogItem({ blog }: Props) {
           )}
         </div>
       </th>
-      <th className="w-[40%] sm:w-[40%] lg:w-[35%] xl:w-[35%] px-2 py-1 font-normal break-words">
+      <th
+        className="w-[40%] sm:w-[40%] lg:w-[35%] xl:w-[35%] px-2 py-1 font-normal break-words"
+        onClick={isDeleting ? undefined : onClickItem.bind(undefined, blog.id)}
+      >
         {getPostExcerpt(blog.title || "", 60)}
       </th>
       {isDeleting ? (
@@ -93,9 +114,9 @@ export default function BlogItem({ blog }: Props) {
           <th className="hidden lg:table-cell w-[20%] lg:w-[10%] xl:w-[7%] font-normal py-2 break-words">
             {blog.likesCount}
           </th>
-          <th className="hidden xl:table-cell xl:w-[7%] font-normal py-2 break-words">
+          {/* <th className="hidden xl:table-cell xl:w-[7%] font-normal py-2 break-words">
             {blog.disLikesCount}
-          </th>
+          </th> */}
         </>
       )}
     </tr>
--- app/(others)/(protect-routes)/content/[type]/Blogs.tsx
+++ app/(others)/(protect-routes)/content/[type]/Blogs.tsx
@@ -1,13 +1,19 @@
 "use client"
 
-import React, { useState, useCallback, useMemo } from "react"
+import React, { useState, useCallback, useMemo, useTransition } from "react"
 import _ from "lodash"
+import {
+  MdOutlineCheckBox,
+  MdOutlineCheckBoxOutlineBlank,
+} from "react-icons/md"
 
 import BlogItem from "./BlogItem"
 import ButtonLoader from "@/components/ButtonLoader"
 import Mask from "@/components/Mask"
+import ConfirmDeleteModal from "../../upload/[id]/ConfirmDeleteModal"
 import { useInfiniteScroll } from "@/hooks/useInfiniteScroll"
 import { combineEdges } from "@/lib/helpers"
+import { deleteManyPublishes } from "@/app/actions/publish-actions"
 import type { FetchPublishesResponse, Maybe } from "@/graphql/codegen/graphql"
 
 interface Props {
@@ -44,6 +50,11 @@ export default function Blogs({ fetchResult }: Props) {
     setPageInfo(fetchResult?.pageInfo)
   }
 
+  const [selectedItems, setSelectedItems] = useState<string[]>([])
+  const [confirmModalVisible, setConfirmModalVisible] = useState(false)
+
+  const [isPending, startTransition] = useTransition()
+
   const fetchMore = useCallback(async () => {
     if (!pageInfo || !pageInfo.endCursor || !pageInfo.hasNextPage) return
 
@@ -71,6 +82,36 @@ export default function Blogs({ fetchResult }: Props) {
   }, [pageInfo])
   const { observedRef } = useInfiniteScroll(0.5, fetchMore)
 
+  const selectItem = useCallback((id: string) => {
+    setSelectedItems((prev) =>
+      prev.includes(id) ? prev.filter((i) => i !== id) : [...prev, id]
+    )
+  }, [])
+
+  const selectAll = useCallback(() => {
+    setSelectedItems((prev) =>
+      prev.length === edges.length
+        ? []
+        : edges.filter((edge) => !!edge.node?.id).map((edge) => edge.node?.id!)
+    )
+  }, [edges])
+
+  const requestDeletion = useCallback(() => {
+    setConfirmModalVisible(true)
+  }, [])
+
+  const cancelDeletion = useCallback(() => {
+    setConfirmModalVisible(false)
+  }, [])
+
+  const confirmDeletePublishes = useCallback(() => {
+    if (selectedItems.length === 0) return
+
+    startTransition(() => deleteManyPublishes(selectedItems))
+    setConfirmModalVisible(false)
+    setSelectedItems([])
+  }, [selectedItems])
+
   return edges?.length === 0 ? (
     <div className="text-center px-4 py-10">
       <h6>No results found.</h6>
@@ -82,42 +123,74 @@ export default function Blogs({ fetchResult }: Props) {
           While processing you can safely leave this page.
         </div>
       )}
-      <table className="table-fixed w-full border-collapse border border-gray-200">
+      <table className="table-fixed w-full border-collapse">
         <thead>
-          <tr className="text-sm font-semibold bg-gray-100">
-            <th className="w-[30%] sm:w-[20%] lg:w-[15%] xl:w-[12%] font-normal py-2 border border-gray-200 break-words">
-              Cover image
+          <tr className="text-sm font-semibold border-t border-b border-neutral-200">
+            <th className="w-[6%] lg:w-[3%]">
+              {edges.length === selectedItems.length ? (
+                <MdOutlineCheckBox
+                  size={24}
+                  onClick={selectAll}
+                  className="cursor-pointer"
+                  style={{ color: "red" }}
+                />
+              ) : (
+                <MdOutlineCheckBoxOutlineBlank
+                  size={24}
+                  onClick={selectAll}
+                  className="cursor-pointer"
+                />
+              )}
             </th>
-            <th className="w-[40%] sm:w-[40%] lg:w-[35%] xl:w-[35%] font-normal py-2 border border-gray-200 break-words">
+            <th className="w-[30%] sm:w-[20%] lg:w-[15%] xl:w-[12%] font-normal py-2 break-words">
+              {selectedItems.length > 0 ? (
+                <button
+                  className="btn-cancel h-5 px-2 text-[10px]"
+                  onClick={requestDeletion}
+                >
+                  Delete? ({selectedItems.length})
+                </button>
+              ) : (
+                "Cover image"
+              )}
+            </th>
+            <th className="w-[40%] sm:w-[40%] lg:w-[35%] xl:w-[35%] font-normal py-2 break-words">
               Title
             </th>
-            <th className="hidden sm:table-cell sm:w-[20%] lg:w-[10%] xl:w-[7%] font-normal py-2 border border-gray-200 break-words">
+            <th className="hidden sm:table-cell sm:w-[20%] lg:w-[10%] xl:w-[7%] font-normal py-2 break-words">
               Visibility
             </th>
-            <th className="w-[30%] sm:w-[20%] lg:w-[10%] xl:w-[11%] font-normal py-2 border border-gray-200 break-words">
+            <th className="w-[30%] sm:w-[20%] lg:w-[10%] xl:w-[11%] font-normal py-2 break-words">
               Date
             </th>
-            <th className="hidden lg:table-cell w-[20%] lg:w-[10%] xl:w-[7%] font-normal py-2 border border-gray-200 break-words">
+            <th className="hidden lg:table-cell w-[20%] lg:w-[10%] xl:w-[7%] font-normal py-2 break-words">
               Views
             </th>
-            <th className="hidden lg:table-cell w-[20%] lg:w-[10%] xl:w-[7%] font-normal py-2 border border-gray-200 break-words">
+            <th className="hidden lg:table-cell w-[20%] lg:w-[10%] xl:w-[7%] font-normal py-2 break-words">
               Tips
             </th>
-            <th className="hidden xl:table-cell w-[20%] xl:w-[7%] font-normal py-2 border border-gray-200 break-words">
+            <th className="hidden xl:table-cell w-[20%] xl:w-[7%] font-normal py-2 break-words">
               Comments
             </th>
-            <th className="hidden lg:table-cell w-[20%] lg:w-[10%] xl:w-[7%] font-normal py-2 border border-gray-200 break-words">
+            <th className="hidden lg:table-cell w-[20%] lg:w-[10%] xl:w-[7%] font-normal py-2 break-words">
               Likes
             </th>
-            <th className="hidden xl:table-cell xl:w-[7%] font-normal py-2 border border-gray-200 break-words">
+            {/* <th className="hidden xl:table-cell xl:w-[7%] font-normal py-2 break-words">
               Dislikes
-            </th>
+            </th> */}
           </tr>
         </thead>
 
         <tbody>
           {edges?.map(({ cursor, node: publish }) =>
-            !publish ? null : <BlogItem key={publish.id} blog={publish} />
+            !publish ? null : (
+              <BlogItem
+                key={publish.id}
+                blog={publish}
+                isSelected={selectedItems.includes(publish.id)}
+                selectItem={selectItem}
+              />
+            )
           )}
         </tbody>
       </table>
@@ -128,6 +201,15 @@ export default function Blogs({ fetchResult }: Props) {
       >
         {loading && <ButtonLoader loading={loading} size={8} color="#d4d4d4" />}
       </div>
+
+      {/* Confirm delete */}
+      {confirmModalVisible && (
+        <ConfirmDeleteModal
+          loading={false}
+          onCancel={cancelDeletion}
+          onConfirm={confirmDeletePublishes}
+        />
+      )}
 
       {/* Prevent interaction while loading */}
       {loading && <Mask />}
