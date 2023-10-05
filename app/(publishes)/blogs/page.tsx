b3b2b7e33392d54989a85a8714f3d4ac5a872b20
autsada <autsada.t@gmail.com>
2023-10-05 12:49:48 +0700

    comments code for debugging

--- app/(publishes)/blogs/page.tsx
+++ app/(publishes)/blogs/page.tsx
@@ -13,46 +13,47 @@ export default async function Page({ searchParams }: Props) {
   const account = data?.account
   const idToken = data?.idToken
 
-  // Get user profile
-  const profile =
-    !account || !idToken || !account.defaultProfile
-      ? undefined
-      : await getProfileById(account?.defaultProfile?.id)
+  // // Get user profile
+  // const profile =
+  //   !account || !idToken || !account.defaultProfile
+  //     ? undefined
+  //     : await getProfileById(account?.defaultProfile?.id)
 
-  const feed = searchParams.feed
+  // const feed = searchParams.feed
 
-  // Fetch blogs (for you)
-  const blogsResult = await fetchPublishes({
-    requestorId: profile?.id,
-    cursor: null,
-    publishType: "blogs",
-  })
+  // // Fetch blogs (for you)
+  // const blogsResult = await fetchPublishes({
+  //   requestorId: profile?.id,
+  //   cursor: null,
+  //   publishType: "blogs",
+  // })
 
-  // Fetch blogs (latest)
-  const latestResult = await fetchPublishes({
-    requestorId: profile?.id,
-    cursor: null,
-    publishType: "blogs",
-    orderBy: "latest",
-  })
+  // // Fetch blogs (latest)
+  // const latestResult = await fetchPublishes({
+  //   requestorId: profile?.id,
+  //   cursor: null,
+  //   publishType: "blogs",
+  //   orderBy: "latest",
+  // })
 
-  // Fetch blogs (popular)
-  const popularResult = await fetchPublishes({
-    requestorId: profile?.id,
-    cursor: null,
-    publishType: "blogs",
-    orderBy: "popular",
-  })
+  // // Fetch blogs (popular)
+  // const popularResult = await fetchPublishes({
+  //   requestorId: profile?.id,
+  //   cursor: null,
+  //   publishType: "blogs",
+  //   orderBy: "popular",
+  // })
 
   return (
     <div className="px-2 sm:px-4 py-2 sm:ml-[100px]">
-      <Blogs
+      {/* <Blogs
         isAuthenticated={!!account}
         feed={feed}
         fetchResult={blogsResult}
         latestResult={latestResult}
         popularResult={popularResult}
-      />
+      /> */}
+      Blog
     </div>
   )
 }
